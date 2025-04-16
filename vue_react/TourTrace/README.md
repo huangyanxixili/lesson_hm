2ec1bfb078d08e71fd9a00bbc11748943af21f85
huangyanxixili <1954058551@qq.com>
2025-04-16 15:31:51 +0800

    TourTrace的router完善

--- vue_react/TourTrace/README.md
+++ vue_react/TourTrace/README.md
@@ -5,3 +5,8 @@
     全新项目，单独 github 新建一个仓库
 - vue + koa + mysql 全栈项目
 
+- TS 就像写 java 一样让我们写 js，有更强大的类型约束
+
+- vue-router
+  - RouteRecordRaw 类型约束
+  - redirect 重定向  
--- vue_react/TourTrace/src/router/index.ts
+++ vue_react/TourTrace/src/router/index.ts
@@ -3,6 +3,16 @@ import {
     createWebHistory
 } from 'vue-router'
 
+// 路由类型 
+import type { RouteRecordRaw } from 'vue-router'
+
+// js 作为弱类型语言 好入门
+// 但是在大型项目里 会有很多问题
+
+// 报错，因为类型不匹配，有隐患
+// let a :number = 1;
+// a = 'a';
+
 const rootRoutes = [
     {
         path: 'home',
@@ -12,22 +22,62 @@ const rootRoutes = [
             cache:true
         },
         component: () => import('../views/Home/Home.vue')
+    },
+    {
+        path: 'assistant',
+        name: 'Assistant',
+        meta:{
+            title: 'ai助手',
+            cache:true
+        },
+        component: () => import('../views/Assistant/Assistant.vue')
+    },
+    {
+        path:'shopping',
+        name: 'Shopping',
+        meta:{
+            title: '商城',
+            cache:true
+        },
+        component: () => import('../views/Shop/Shopping.vue')
+    },
+    {
+        path:'mine',
+        name: 'Mine',
+        meta:{
+            title: '个人中心',
+            cache:false
+        },
+        component: () => import('../views/My/Mine.vue')
     }
 ]
 
-const routes = [
+// 类型约束可以让代码正确
+const routes :RouteRecordRaw[]= [
     {
         path: '/',
         name: 'App',
         component: () => import('../views/KeepAlive.vue'),
-        redirect: '/home',
+        redirect: '/home', // 重定向
         children: rootRoutes
+    },
+    {
+        path:'/login',
+        name: 'Login',
+        component: () => import('../views/Login.vue'),
     }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next)=>{
+    // as 断言 告诉ts 这个类型一定是string
+    document.title = to.meta.title as string;
+    next()
+
 })
 
 export default router
